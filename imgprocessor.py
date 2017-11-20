a6eae45fb477d4f48a6270158496a51914622360
Leonardo Isso <leoisso.work@gmail.com>
2017-11-20 15:24:52 -0200

    decode fixed

--- imgprocessor.py
+++ imgprocessor.py
@@ -21,7 +21,7 @@ def decodeImage():
         Not used in this app, but you can use to see which image is being built
     '''
     string = B64_STRING
-    byte_string = base64.b64decode(string)
+    byte_string = base64.b64decode(string.encode('utf-8'))
     file = open('build.jpg', 'wb')
     file.write(byte_string)
     file.close()
