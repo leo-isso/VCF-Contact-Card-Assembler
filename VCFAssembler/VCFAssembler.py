aa46710d2cee3aa261f9ae4adf53a36b4e757a6b
Leonardo Isso <leoisso.work@gmail.com>
2019-06-28 16:25:30 -0300

    Add VCF version

--- VCFAssembler/VCFAssembler.py
+++ VCFAssembler/VCFAssembler.py
@@ -5,9 +5,10 @@ class VCFAssembler(metaclass=ABCMeta):
     VCARD_START =  'BEGIN:VCARD'
     VCARD_END =  'END:VCARD'
 
-    def __init__(self, name, **kwargs):
+    def __init__(self, version, name, **kwargs):
         self.vcf_card = ''
         self.vcf_body = ''
+        self.version = version
         self.name = name
         self.treatment = kwargs['treatment']
         self.phones = kwargs['phones']
@@ -17,6 +18,10 @@ class VCFAssembler(metaclass=ABCMeta):
     def add_to_body(self, line):
         self.vcf_body += f'\n{line}'
 
-    def set_name(self):
-        vcf_name = ''
-        pass
+    def set_version(self):
+        if self.version == '2.1':
+            self.add_to_body('VERSION:2.1')
+        elif self.version == '3.0':
+            self.add_to_body('VERSION:3.0')
+        else:
+          raise 'Invalid Version'
