daac5a960695a88f98934fe3dbb9d236604cb77a
Leonardo Isso <leoisso.work@gmail.com>
2019-06-28 18:00:43 -0300

    Add optional break line and fix line breaks

--- VCFAssembler/VCFAssembler.py
+++ VCFAssembler/VCFAssembler.py
@@ -14,8 +14,10 @@ class VCFAssembler(metaclass=ABCMeta):
         self.email = kwargs['email']
         self.photo = kwargs['photo']
 
-    def add_to_body(self, line):
-        self.vcf_body += f'\n{line}'
+    def add_to_body(self, line, break_line):
+        if break_line:
+            self.vcf_body += '\n'
+        self.vcf_body += f'{line}'
     
     @classmethod
     def set_review(self):
@@ -31,9 +33,9 @@ class VCFAssembler(metaclass=ABCMeta):
 
     def set_version(self):
         if self.version == '2.1':
-            self.add_to_body('VERSION:2.1')
+            self.add_to_body('VERSION:2.1', True)
         elif self.version == '3.0':
-            self.add_to_body('VERSION:3.0')
+            self.add_to_body('VERSION:3.0', True)
         else:
             raise 'Invalid Version'
 
@@ -46,9 +48,12 @@ class VCFAssembler(metaclass=ABCMeta):
         vcf_surname = f'{surname};'
         vcf_treatment = f';{treatment};'
         
-        self.add_to_body(f'N:{vcf_surname}{vcf_name}{vcf_treatment}')
-        self.add_to_body(f'FN:{surname} {name}')
+        self.add_to_body(f'N:{vcf_surname}{vcf_name}{vcf_treatment}', True)
+        self.add_to_body(f'FN:{surname} {name}', True)
 
     def build_vcf_body(self):
+        self.add_to_body(self.VCARD_START, False)
         self.set_version()
         self.set_name()
+        self.add_to_body(self.VCARD_START, True)
+        
