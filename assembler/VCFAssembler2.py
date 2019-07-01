059afbfc7440f358fc3ec519d2e81dd63bc3dcbd
Leonardo Isso <leoisso.work@gmail.com>
2019-06-30 23:27:17 -0300

    Add assembler for VCards 2.1

---
+++ assembler/VCFAssembler2.py
@@ -0,0 +1,23 @@
+from assembler.VCFAssembler import VCFAssembler
+from image_encoder.image_encoder import encode_image_file
+
+from time import gmtime, strftime
+
+class VCFAssembler2(VCFAssembler):
+
+    def set_review(self):
+      self.revision = strftime("%Y%m%dT%H%M%SZ", gmtime())
+      vcf_revision = f'REV:{self.revision}'
+      self.add_to_body(vcf_revision)
+
+    def set_phones(self):
+        for phone in self.phones:
+            phone_type = phone['type']
+            phone_number = phone['number']
+            vcf_phone = f'TEL;{phone_type};VOICE:{phone_number}'
+            self.add_to_body(vcf_phone)
+
+    def set_image(self):
+        image = encode_image_file(self.image)
+        vcf_image = f'PHOTO;JPEG;ENCODING=BASE64:{image}'
+        self.add_to_body(vcf_image)
