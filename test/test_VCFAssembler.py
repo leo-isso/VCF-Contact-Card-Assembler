2861bfa70c40cdfc73dcdc57d75c2739fafd0ce6
Leonardo Isso <leoisso.work@gmail.com>
2019-06-30 22:27:05 -0300

    Change variable name

--- test/test_VCFAssembler.py
+++ test/test_VCFAssembler.py
@@ -12,17 +12,17 @@ class TestVCFAssembler(unittest.TestCase):
             'treatment': 'Mr.'
         }
 
-        self.assemble = VCFAssembler(version, name)
+        self.assembler = VCFAssembler(version, name)
 
     def test_set_version(self):
         result = """\nVERSION:3.0"""
-        self.assemble.set_version()
-        self.assertEquals(self.assemble.vcf_body, result)
+        self.assembler.set_version()
+        self.assertEquals(self.assembler.vcf_body, result)
 
     def test_set_name(self):
         result = """\nN:Isso;Leonardo;;Mr.;\nFN:Isso Leonardo"""
-        self.assemble.set_name()
-        self.assertEquals(self.assemble.vcf_body, result)
+        self.assembler.set_name()
+        self.assertEquals(self.assembler.vcf_body, result)
 
 
 if __name__ == "__main__":
