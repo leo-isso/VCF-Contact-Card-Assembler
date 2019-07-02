bb2a51df1cfc3d8121941d8f2aba7b727033048a
Leonardo Isso <leoisso.work@gmail.com>
2019-07-02 14:21:34 -0300

    Remove abstract class tests

--- test/TestSuite.py
+++ test/TestSuite.py
@@ -1,11 +1,8 @@
 import unittest
-from test.test_assembler.test_VCFAssembler import TestVCFAssembler
 from test.test_assembler.test_VCFAssembler2 import TestVCFAssembler2
 
-suite_VCFAssembler = unittest.TestLoader().loadTestsFromTestCase(TestVCFAssembler)
 suite_VCFAssembler2 = unittest.TestLoader().loadTestsFromTestCase(TestVCFAssembler2)
 
-all_tests = unittest.TestSuite([suite_VCFAssembler,
-                              suite_VCFAssembler2])
+all_tests = unittest.TestSuite([suite_VCFAssembler2])
 
 unittest.TextTestRunner().run(all_tests)
