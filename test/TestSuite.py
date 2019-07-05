e66db018e5b6c95643ad532335c6fd9f2d13bd4e
Leonardo Isso <leoisso.work@gmail.com>
2019-07-04 23:41:47 -0300

    Update tests suite

--- test/TestSuite.py
+++ test/TestSuite.py
@@ -1,8 +1,10 @@
 import unittest
 from test.test_assembler.test_VCFAssembler2 import TestVCFAssembler2
+from test.test_assembler.test_VCFAssembler3 import TestVCFAssembler3
 
 suite_VCFAssembler2 = unittest.TestLoader().loadTestsFromTestCase(TestVCFAssembler2)
+suite_VCFAssembler3 = unittest.TestLoader().loadTestsFromTestCase(TestVCFAssembler3)
 
-all_tests = unittest.TestSuite([suite_VCFAssembler2])
+all_tests = unittest.TestSuite([suite_VCFAssembler2, suite_VCFAssembler3])
 
 unittest.TextTestRunner().run(all_tests)
